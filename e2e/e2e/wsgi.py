497653fe387249c3865d8c888bba2d2f11ef64c4
martinjohny167 <martinjohny29@gmail.com>
2024-12-04 00:54:24 -0500

    Update wsgi.py

--- e2e/e2e/wsgi.py
+++ e2e/e2e/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e2e.e2e.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e2e.settings')
 
 application = get_wsgi_application()
