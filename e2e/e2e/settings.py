b3529a030de34d73d471f3f6530a0869788452df
martinjohny167 <martinjohny29@gmail.com>
2024-12-04 04:43:46 -0500

    Update settings.py

--- e2e/e2e/settings.py
+++ e2e/e2e/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+import django_heroku
+
+# Activate Django-Heroku settings.
+django_heroku.settings(locals())
+
