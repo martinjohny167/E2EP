1b08772449e5e6a1d1b52116df31541d2c13d909
martinjohny167 <martinjohny29@gmail.com>
2024-12-04 04:18:48 -0500

    Update settings.py

--- e2e/e2e/settings.py
+++ e2e/e2e/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'employee',  # Your database name
         'USER': 'root',  # MySQL username
         'PASSWORD': 'Martin1999!',  # MySQL password
-        'HOST': '35.171.2.101',  # Replace with the public IP address of your EC2 instance
+        'HOST': '44.202.138.221',  # Replace with the public IP address of your EC2 instance
         'PORT': '3306',  # Default MySQL port
     },
 }
